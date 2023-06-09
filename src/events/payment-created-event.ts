469e244ba4c84ab5f185d2d024f66c372ad25554
ernest_k8s-ubuntu <240509865e@gmail.com>
2023-06-09 11:15:15 +0000

    Updates

--- src/events/payment-created-event.ts
+++ src/events/payment-created-event.ts
@@ -1,10 +1,10 @@
 import {Subjects} from "./subjects";
 
 export interface PaymentCreatedEvent {
-    subjects: Subjects.PaymentCreated;
+    subject: Subjects.PaymentCreated;
     data: {
         id: string;
         orderId: string;
         stripeId: string;
-    }
+    };
 }
